f4d053ec3597af55958e8c9c6e95b05295133013
Gabriel Santos Faria <fariamg4@gmail.com>
2025-05-09 17:16:18 -0300

    Refactor Heap class constructor and destructor for clarity; add method stubs for Inserir and Remover

--- C++/src/heap.cpp
+++ C++/src/heap.cpp
@@ -2,12 +2,14 @@
 
 Heap::Heap(int size) {
     this->tamanho = size;
-    this->data = new int[tamanho](); //O () no final inicializa o array com 0's
+    this->data = new int[tamanho](); // O () no final inicializa o array com 0's
 }
 
-Heap::~Heap() {
-    delete[] this->data;
-}
+Heap::~Heap() { delete[] this->data; }
+
+void Heap::Inserir(int x) {}
+
+int Heap::Remover() {}
 
 bool Heap::Vazio() {
     for (int i = 0; i < this->tamanho; ++i) {
@@ -17,3 +19,14 @@ bool Heap::Vazio() {
     }
     return true;
 }
+
+// Retorna o index do pai
+int Heap::GetAncestral(int posicao) { return (posicao - 1) / 2; }
+
+int Heap::GetSucessorDir(int posicao) { return posicao * 2 + 1; }
+
+int Heap::GetSucessorEsq(int posicao) { return posicao * 2 + 2; }
+
+void Heap::HeapifyPorBaixo(int posicao) {}
+
+void Heap::HeapifyPorCima(int posicao) {}
