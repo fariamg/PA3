5043521fcd05b337c8b83827b0853eb8492b8c73
Gabriel Santos Faria <fariamg4@gmail.com>
2025-05-09 16:38:32 -0300

    Implement Vazio method to check if the heap is empty

--- C++/src/heap.cpp
+++ C++/src/heap.cpp
@@ -2,9 +2,18 @@
 
 Heap::Heap(int size) {
     this->tamanho = size;
-    this->data = new int[tamanho];
+    this->data = new int[tamanho](); //O () no final inicializa o array com 0's
 }
 
 Heap::~Heap() {
     delete[] this->data;
+}
+
+bool Heap::Vazio() {
+    for (int i = 0; i < this->tamanho; ++i) {
+        if (this->data[i] != 0) {
+            return false;
+        }
+    }
+    return true;
 }
